a7052991700c5240b992bc7999528657c05153a9
bmsf <fromreide94@gmail.com>
2022-09-15 13:33:08 +0200

    navbar

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('.menu-box');
+const openMenu = document.querySelector('.links');
+const closeMenuButton = document.querySelector('#open-box');
+const body = document.querySelector('body');
+
+console.log(menu);
+
+const toggleMenu = () => {
+	body.style.overflow = 'hidden';
+	openMenu.classList.toggle('nav-links-active');
+};
+
+menu.addEventListener('click', toggleMenu);
+
+const closeMenu = () => {
+	body.style.overflowY = 'visible';
+	openMenu.classList.toggle('nav-links-active');
+};
+
+closeMenuButton.addEventListener('click', closeMenu);
